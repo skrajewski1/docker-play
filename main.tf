7c28c841744494435b38290e5e3c0dbfd2b4471e
skrajewski1 <sean.krajewski@bonitahealthcare.com>
2024-05-03 23:41:20 -0500

    updates

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ provider "aws" {
 #data for amazon linux
 
 data "aws_ami" "amazon-2" {
-    most_recent = true
-  
-    filter {
-      name = "name"
-      values = ["amzn2-ami-hvm-*-x86_64-ebs"]
-    }
-    owners = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
- 
+  owners = ["amazon"]
+}
+
 #create ec2 instances 
 
 resource "aws_instance" "DockerInstance" {
@@ -24,12 +24,12 @@ resource "aws_instance" "DockerInstance" {
   key_name               = aws_key_pair.ec2_key.key_name
   user_data              = file("install.sh")
   root_block_device {
-    volume_size = 30  
-    volume_type = "gp2"  
+    volume_size = 30
+    volume_type = "gp2"
   }
 
   tags = {
     Name = "docker-instance"
   }
- 
+
 }
--- ssh_key.tf
+++ ssh_key.tf
@@ -5,12 +5,12 @@ resource "tls_private_key" "ec2_key" {
 }
 # Create the Key Pair
 resource "aws_key_pair" "ec2_key" {
-  key_name   = "docker-keypair"  
+  key_name   = "docker-keypair"
   public_key = tls_private_key.ec2_key.public_key_openssh
 }
 # Save file
 resource "local_file" "ssh_key" {
-  filename = "${aws_key_pair.ec2_key.key_name}.pem"
-  content  = tls_private_key.ec2_key.private_key_pem
+  filename        = "${aws_key_pair.ec2_key.key_name}.pem"
+  content         = tls_private_key.ec2_key.private_key_pem
   file_permission = "400"
 }
